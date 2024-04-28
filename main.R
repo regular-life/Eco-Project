a471ecd00cedd92dd5294ee7d130b1102336afce
regular-life <yash22586@iiitd.ac.in>
2024-04-28 23:13:11 +0530

    git push

---
+++ main.R
@@ -0,0 +1,15 @@
+data <- read.csv("filtered_gwq_sdp_gini.csv")
+
+# Run regression
+model <- lm(chloride ~ sdp + I(sdp^2) + I(sdp^3) + gini, data = data)
+
+# Summary of the regression results
+summary(model)
+# Extract residuals from the model object
+residuals <- residuals(model)
+
+# Calculate sum of residuals
+sum_of_residuals <- sum(residuals)
+
+# View sum of residuals
+print(sum_of_residuals)
